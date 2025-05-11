2e6c35b2d42ebf4a4ace89993bd997140814b57b
KVK <macbook@maegbug-ui-MacBookPro.local>
2025-05-11 19:48:12 +0900

    fix(web): correct component case sensitivity for Vercel build

--- web/layouts/default.tsx
+++ web/layouts/default.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/Header.tsx';
+import Footer from '../components/Footer.tsx';
 
 type DefaultLayoutProps = {
   children: ReactNode;
