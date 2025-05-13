74fd7e628d332e9cf44ca52f78a4de9eb117cc7a
KVK <macbook@maegbug-ui-MacBookPro.local>
2025-05-13 11:48:31 +0900

    fix(auth): use createPagesBrowserClient instead of createBrowserClient

--- web/lib/supabaseClient.ts
+++ web/lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
-export const createClient = (): SupabaseClient => {
-  return createBrowserClient(
+export const createClient = (): SupabaseClient =>
+  createPagesBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-};
